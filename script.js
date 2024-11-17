f8621a4bc23654f883a726f4cbe2e4711652fad8
Yamini Manral <43371452+yamini-pixel@users.noreply.github.com>
2024-11-17 14:25:03 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Welcome to my website!");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
